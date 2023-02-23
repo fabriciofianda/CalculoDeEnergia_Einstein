28a6c93f2dcd9afbd5d3dfe1992291e773319721
Fabrício de Figueiredo Miranda <125789205+fabriciofianda@users.noreply.github.com>
2023-02-23 03:00:24 -0300

    Add files via upload

--- codigo - TRADUZIDO.py
+++ codigo - TRADUZIDO.py
@@ -3,7 +3,7 @@ import math
 massa = float(input("Digite a massa da partícula em kg: "))
 velocidade = float(input("Digite a velocidade da partícula em m/s: "))
 
-momentum = massa * velocidade
+momento = massa * velocidade
 
-energia = math.sqrt((massa * (3*10**8)**2)**2 + momentum**2)
+energia = math.sqrt((massa * (3*10**8)**2)**2 + momento**2)
 print("A energia total da partícula é de: ", energia, " J")
